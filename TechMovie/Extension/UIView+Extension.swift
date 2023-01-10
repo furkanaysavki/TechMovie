04c208253ceea7ee391076a9746def2d56ccf009
furkanaysavki <furkanaysavki@gmail.com>
2023-01-10 23:03:40 +0300

    UIView extension updated

--- TechMovie/Extension/UIView+Extension.swift
+++ TechMovie/Extension/UIView+Extension.swift
@@ -64,4 +64,23 @@ extension UIView {
             }
         }
     }
+    @IBInspectable
+    var borderWidth: CGFloat {
+            get {
+                return layer.borderWidth
+            }
+            set {
+                layer.borderWidth = newValue
+            }
+        }
+        
+    @IBInspectable
+    var borderColor: UIColor? {
+            get {
+                return UIColor(cgColor: layer.borderColor!)
+            }
+            set {
+                layer.borderColor = newValue?.cgColor
+            }
+        }
 }
